feb4c66e38f9351ecb3c52b08bbb1a446ec9e6b2
pi-chan <hiromasa.ohno.1007@gmail.com>
2020-04-23 16:19:06 +0900

    メニューにする

--- battery-monitor/AppDelegate.swift
+++ battery-monitor/AppDelegate.swift
@@ -11,40 +11,56 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     var window: NSWindow!
     var popover: NSPopover!
     var statusBarItem: NSStatusItem!
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-        
+
+        // Create the popover
         let popover = NSPopover()
         popover.contentSize = NSSize(width: 400, height: 400)
         popover.behavior = .transient
         popover.contentViewController = NSHostingController(rootView: contentView)
         self.popover = popover
         
-        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
         if let button = self.statusBarItem.button {
-             button.image = NSImage(named: "Icon")
-             button.action = #selector(togglePopover(_:))
+            button.image = NSImage(named: "Icon")
+        }
+        
+        let statusBarMenu = NSMenu(title: "Cap Status Bar Menu")
+        statusBarItem.menu = statusBarMenu
+        
+        statusBarMenu.addItem(
+            withTitle: "Preferences",
+            action: #selector(AppDelegate.togglePopover),
+            keyEquivalent: "")
+        
+        statusBarMenu.addItem(
+            withTitle: "Quit",
+            action: #selector(AppDelegate.quit),
+            keyEquivalent: "")
+        
+        NSApp.activate(ignoringOtherApps: true)
+
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
         }
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    @objc func togglePopover(_ sender: AnyObject?) {
-         if let button = self.statusBarItem.button {
-              if self.popover.isShown {
-                   self.popover.performClose(sender)
-              } else {
-                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-              }
-         }
+    
+    @objc func quit(_ sender: AnyObject?) {
+        NSApplication.shared.terminate(self)
     }
 }
 
--- battery-monitor/ContentView.swift
+++ battery-monitor/ContentView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack(alignment: .leading) {
+            Button(action: {}) {
+                Text("Quit")
+            }
+            Text("Hello, World!")
+                .multilineTextAlignment(.trailing)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
 }
 
